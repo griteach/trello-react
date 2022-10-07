09168148dbd76f974b49d1ace5efd1cfb6d80e98
griteach <griteach22@gmail.com>
2022-10-07 22:59:40 +0900

    ...

--- src/secondAtoms.tsx
+++ src/secondAtoms.tsx
@@ -2,11 +2,15 @@ import { atom } from "recoil";
 
 export const TRELLO_TODO = "TRELLO_TODO";
 
-const localStorageTodo = localStorage.getItem(TRELLO_TODO) || "{}";
+const localStorageTodo: string = localStorage.getItem(TRELLO_TODO) || "{}";
 
 const parsedLocalStorageTodo = JSON.parse(localStorageTodo);
 
 export const secondToDoState = atom({
   key: "toDo",
-  default: parsedLocalStorageTodo,
+  default: {
+    to_do: ["a", "b", "c", "d", "e"],
+    doing: [],
+    done: [],
+  },
 });
